dfe5eef53cfa02c84ecbce6e0827753997c970c7
Jacob <jacob.nance97@gmail.com>
2018-04-13 18:50:26 -0500

    Added basic starting point for packages and server

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var chalk = require('chalk');
+var app = express();
+var router = express.Router();
+
+router.get('/test', function(req, res) 
+{
+    res.status(200).send('Hello world');
+});
+
+app.use('/api', router);
+
+app.listen(9001, function(err) 
+{
+    if (err) 
+    {
+        console.log(chalk.red(err));
+    } 
+    else 
+    {
+        console.log(chalk.blue('PORT IS OVER 9000!'));
+    }
+});
