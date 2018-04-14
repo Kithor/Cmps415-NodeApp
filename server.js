7c16c762e344a4e9477e67826a3e13148342a2b5
Jacob <jacob.nance97@gmail.com>
2018-04-14 11:18:44 -0500

    Changes to help read req

--- server.js
+++ server.js
@@ -1,43 +1,64 @@
 var express = require('express');
+var parse = require('url-parse');
 var chalk = require('chalk');
 var app = express();
 var router = express.Router();
 
 const port = process.env.PORT || 3000;
 
+//initial
+app.get('/', (req, res) => {
+    res.status(200).send('Hello');
+});
+
 //Test case
-router.get('/test', (req, res) => {
-    return res.status(200).send('Hello world');
+app.get('/test', function (req, res){
+    res.status(200).send('Hello world');
 });
 
 //Get all EMRs
-router.get('/rest/emr', (res) => {
-    return res.status(200).send().json(emrs);    //Success status
+app.get('/rest/emr', (req,res) => {
+    res.status(200).send(emrs);    //Success status
 });
 
 //Get a single EMR
-router.get('/rest/emr/id', (req,res) => {
-    const id = req.body.id;
+app.get('/rest/emr/id', (req,res) => {
+    var p = parse(req.url, true);
+    var q = p.query;
     var patient;
     var found = false;
 
     for(var i=0; i<emrs.EMRS.length;i++){
-        if(emrs.EMRS[i].patientID == id){
-            patient = emrs[i];
+        if(emrs.EMRS[i].patientID == q.id){
+            patient = emrs.EMRS[i];
             found = true;
         }
     }
     if(!found){
-        return res.status(404).send('Patient not found'); //Fail status
+        res.status(404).send('Patient not found'); //Fail status
     }
     else{
-        return res.status(200).send(patient);
+        res.status(200).send(patient);
     }
 });
 
 //Post a single EMR
-router.post('/rest/emr', (req,res) => {
-    
+app.post('/rest/emr', (req,res) => {
+    var p = parse(req.url, true);
+    var q = p.query;
+    let newEMR = {
+        "patientName": q.patientName,        
+        "patientID": q.patientID,
+        "bloodType": q.bloodType
+    };
+
+    try{
+        emrs['EMRS'].push(newEMR);
+        res.status(200).send('Patient was added successfully');
+    }
+    catch(err){
+        res.status(406).send('Something went wrong');
+    }
 });
 
 app.use('/api', router);
@@ -54,5 +75,6 @@ var emrs = '{ "EMRS" : [' +
 '{ "patientName":"Olivier Rusty" , "patientID":"40725" , "bloodType":"O Positive" },' +
 '{ "patientName":"Benjamin Kent" , "patientID":"98123" , "bloodType":"B Negative" },' +
 '{ "patientName":"Julie Rogers" , "patientID":"44199" , "bloodType":"A Positive" },' +
-'{ "patientName":"Alice Smalls" , "patientID":"20164" , "bloodType":"AB Negative },' +
-'{ "patientName":"Reggie Vence" , "patientID":"40725" , "bloodType":"O Negative" } ]}';
+'{ "patientName":"Alice Smalls" , "patientID":"20164" , "bloodType":"AB Negative" },' +
+'{ "patientName":"Reggie Vence" , "patientID":"44685" , "bloodType":"O Negative" } ]}';
+emrs = JSON.parse(emrs);
